4a7fe72bedd1fdb2a8884ac8389ca1877ba3d690
nero-15 <ti7805@gmail.com>
2020-05-15 15:57:39 +0900

    add dev file

---
+++ src/number-string.ts
@@ -0,0 +1,11 @@
+export {}
+
+let year: number =1976;
+//year = false;
+console.log(year);
+
+let age =0x2b;
+console.log(age);
+
+let name: string = "takuya";
+console.log(name);
---
+++ src/type.ts
@@ -0,0 +1,18 @@
+export{}
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1, 2, 3];
+
+let strings: string[] = ['typescript', 'javascript'];
+
+console.log(numbers);
+console.log(numbers2);
+console.log(strings);
+
+let hauiretu: (string | number | boolean)[] = [1, false, 'Japan'];
+
+console.log(hauiretu);
+
+let profile: [string, number] = ['takuya', 29];
+profile = [27, 'taku'];
